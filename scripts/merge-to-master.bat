759ce0255c28a3cc80051ffc78e76d8265177217
nivi.mor <nivi.mor@experitest.com>
2017-12-06 12:39:26 +0200

    preparing for test

--- scripts/merge-to-master.bat
+++
@@ -1,6 +0,0 @@
-SET PROJECT_PATH="%HOMEPATH\C:\Users\nivi.mor\AndroidStudioProjects\eribank-espresso%"
-cd %PROJECT_PATH%
-git checkout master
-for /f %%i in ('git merge fix_payment') do set VAR=%%i
-echo %VAR%
-
