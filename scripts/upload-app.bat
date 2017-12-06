46730b0e4097d75731649175cd7fc0436333aa33
nivi.mor <nivi.mor@experitest.com>
2017-12-06 14:47:02 +0200

    configured application upload

--- scripts/upload-app.bat
+++ scripts/upload-app.bat
@@ -1,3 +1,3 @@
 SET APP_PATH="file=@%cd%\app\build\outputs\apk\app-debug.apk"
 
-curl -k -X POST https://stage.seetest.io/api/v1/applications/new -H "Authorization: Basic bml2aTpuaXZpMUFkbWlu" -H "Cache-Control: no-cache" -H "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F APP_PATH
+curl -k -X POST https://stage.seetest.io/api/v1/applications/new -H "Authorization: Basic bml2aTpuaXZpMUFkbWlu" -H "Cache-Control: no-cache" -H "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F %APP_PATH%
