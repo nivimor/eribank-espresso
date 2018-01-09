6ca3c0ebd1416135c779e4c637720a86db729de3
nivi.mor <nivi.mor@experitest.com>
2018-01-09 15:55:36 +0200

    added country

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -2,7 +2,7 @@ echo ${TRAVIS_BRANCH}
 echo "switching to master"
 git checkout -b master
 echo "pulling master"
-git pull master
+git pull origin master
 echo "merging ${TRAVIS_BRANCH} to master"
 git merge ${BRANCH}
 git commit -am "${TRAVIS_COMMIT_MESSAGE} and merged to master"
