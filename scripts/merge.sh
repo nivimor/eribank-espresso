770b1fb2e3d3c698843cc6a89829ce02b2e54786
nivi.mor <nivi.mor@experitest.com>
2018-01-09 15:41:51 +0200

    added country

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,6 +1,10 @@
 echo ${TRAVIS_BRANCH}
+echo "switching to master"
 git checkout -b master
-git pull origin master
+echo "pulling master"
+git pull master
+echo "merging ${TRAVIS_BRANCH} to master"
 git merge ${BRANCH}
 git commit -am "${TRAVIS_COMMIT_MESSAGE} and merged to master"
+echo "pushing to master"
 git push https://${GIT_AUTH}@github.com/nivimor/eribank-espresso.git master --force
