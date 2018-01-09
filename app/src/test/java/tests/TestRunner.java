fcde72deea1787d28641e0b39f460f38c8f9d6c2
nivi.mor <nivi.mor@experitest.com>
2018-01-09 10:30:35 +0200

    configured android version and fixed git merging

--- app/src/test/java/tests/TestRunner.java
+++ app/src/test/java/tests/TestRunner.java
@@ -1,6 +1,14 @@
 package tests;
 
 import org.junit.Test;
+import org.junit.experimental.ParallelComputer;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.List;
+
+import static java.lang.System.exit;
 
 public class TestRunner {
 
@@ -9,24 +17,24 @@ public class TestRunner {
 
         System.out.println("yes");
 
-//        Class[] cls = {
-//                EriBankPaymentTest.class,
-//                EriBankPaymentTest.class,
-//                EriBankPaymentTest.class,
-//                EriBankPaymentTest.class,
-///               EriBankPaymentTest.class,
-///               EriBankPaymentTest.class,
-//                };
-//
-//        //Parallel among classes
-//        Result result = JUnitCore.runClasses(ParallelComputer.classes(), cls);
-//        List<Failure> failures = result.getFailures();
-//        if(!failures.isEmpty()){
-//            for(Failure failure: failures){
-//                System.out.println(failure);
-//
-//            }
-//            exit(1)
+        Class[] cls = {
+                EriBankPaymentTest.class,
+                EriBankPaymentTest.class,
+                /*EriBankPaymentTest.class,
+                EriBankPaymentTest.class,
+                EriBankPaymentTest.class,
+                EriBankPaymentTest.class,*/
+                };
+
+        //Parallel among classes
+        Result result = JUnitCore.runClasses(ParallelComputer.classes(), cls);
+        List<Failure> failures = result.getFailures();
+        if(!failures.isEmpty()){
+            for(Failure failure: failures){
+                System.out.println(failure);
+
+            }
+            exit(1);
 
     }
 }
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,4 +1,4 @@
-git checkout master
+git checkout -t -b master origin/master
 git merge ${TRAVIS_BRANCH}
 git commit -am "${TRAVIS_COMMIT} and merged to master"
 git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/nivimor/eribank-espresso.git master
