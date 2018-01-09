0346541514f1866319ae335f7ad8e04d889ff4a4
nivi.mor <nivi.mor@experitest.com>
2018-01-09 13:20:15 +0200

    fixed tests for travis, fixed evn vars, fixed git push

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,6 +1,7 @@
 git config user.name "${GIT_USERNAME}"
 git config user.password "${GIT_PASSWORD}"
+BRANCH=${TRAVIS_BRANCH}
 git checkout -b master
-git merge ${TRAVIS_BRANCH}
+git merge ${BRANCH}
 git commit -am "${TRAVIS_COMMIT_MESSAGE} and merged to master"
 git push origin master
