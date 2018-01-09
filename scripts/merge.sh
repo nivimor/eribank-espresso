8808397a027eeff6119ce2b5697fcc46b17aad55
nivi.mor <nivi.mor@experitest.com>
2018-01-09 13:33:26 +0200

    fixed tests for travis, fixed evn vars, fixed git push

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -4,4 +4,4 @@ BRANCH=${TRAVIS_BRANCH}
 git checkout -b master
 git merge ${BRANCH}
 git commit -am "${TRAVIS_COMMIT_MESSAGE} and merged to master"
-git push origin master
+git push https://${GIT_AUTH}@github.com/nivimor/eribank-espresso.git master
